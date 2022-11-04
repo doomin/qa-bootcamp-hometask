49467a7f81c5554a4a9cede230861da4982ab450
doomin <dominik.pszczola@gmail.com>
2022-11-04 13:10:48 +0100

    Ading tests for remaining functions

--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -1,5 +1,15 @@
-import {getRectanglePerimeter} from '../js/rectangle';
+import {getRectanglePerimeter, getRectangleArea, getRectangleInfo} from '../js/rectangle';
 
 test('Should return rectangle parameter as 8', () => {
     expect(getRectanglePerimeter(2,2)).toBe(8);
+});
+
+test('Should return rectangle area as 4', () => {
+    expect(getRectangleArea(2,2)).toBe(4);
+});
+
+test('Should return correct rectangle info', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    getRectangleInfo(2,2);
+    expect(consoleSpy).toHaveBeenCalledWith(`The perimeter of a rectangle is 8 and the area is 4`);
 });
